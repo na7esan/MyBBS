1c6e23fd41388e55d9f1589f6f0f57dcfdc179b6
masataka watada <na7esan@gmail.com>
2021-07-23 02:13:46 +0900

    first-commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index() {
+        // $posts=Post::all();
+        // $posts=Post::orderBy('created_at','desc')->get();
+        $posts=Post::latest()->get();
+
+
+    return view('index')
+        ->with(['posts' => $posts]);
+    }
+
+    public function show(Post $post)
+    {
+
+        return view('posts.show')
+            ->with(['post' => $post]);
+    }
+}
